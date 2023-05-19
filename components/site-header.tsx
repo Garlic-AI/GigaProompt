02ab927b19f7b126eb6f5fb68ff046fb2fcc08cb
tanveer <tanveerctg2014@gmail.com>
2023-05-19 14:06:04 +0600

    small issue fixed

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -96,7 +96,7 @@ export function SiteHeader() {
                         <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
                           {/* <img src={session?.user?.image} className="rounded-full w-[32px] h-[32px]"/> */}
                           <Image
-                            alt={session?.user?.email}
+                            alt={session?.user?.email || 'profile pic'}
                             src={session?.user?.image || `https://avatars.dicebear.com/api/micah/${session?.user?.email}.svg`}
                             width={40}
                             height={40}
